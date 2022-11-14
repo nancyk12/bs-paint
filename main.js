a674846c1f247522a54ec6df152d6639eb9b66ce
nancyk12 <nancyk12@hotmail.com>
2022-11-13 19:49:38 -0500

    update files

--- main.js
+++ main.js
@@ -48,8 +48,15 @@ while (count <= gridWidth * gridWidth) {
 
 // Add queries for all your squares, palette colors, and brush here.
 // (Note the singular or plural used in that sentence!)
+let brush = document.querySelector('.current-brush');
 
+let palette = document.querySelectorAll('.palette div');
 
+let canvasSquares = document.querySelectorAll('.canvas div');
+
+let app = document.querySelector('.app');
+
+let isMouseDown = false;
 
 /****************************
  * EVENT LISTENER FUNCTIONS *
@@ -60,6 +67,55 @@ while (count <= gridWidth * gridWidth) {
 // empty at first, though a console.log just to know they're being
 // run as event listeners (after the next step is set up) isn't a
 // bad idea for testing purposes.
+
+brush.addEventListener('click', function(){
+  console.log("brush!");
+})
+
+for(let i = 0; i < palette.length; i++){
+  palette[i].addEventListener('click', function(){
+    console.log(palette[i].classList);
+    //brush.classList.replace(current color to change, newColor)
+    brush.classList.replace(brush.classList[1],palette[i].classList[1]);
+  })
+}
+
+/*canvasSquares.addEventListener('click', function(){
+  console.log("canvas!");
+})*/
+
+for(let i = 0; i < canvasSquares.length; i++){
+  canvasSquares[i].addEventListener('mouseover', function(){
+    if(isMouseDown === true){
+    console.log(canvasSquares[i].classList);
+    
+    //canvasSquares.classList.replace(current color to change, newColor)
+    canvasSquares[i].classList.replace(canvasSquares[i].classList[1],brush.classList[1]);
+    }
+  })
+}
+
+
+
+app.addEventListener('mousedown', function(){
+  console.log('Mouse is down');
+  isMouseDown = true;
+  console.log('isMouseDown: ' + isMouseDown);
+})
+
+app.addEventListener('mouseup', function(){
+  console.log('Mouse is up');
+  isMouseDown = false;
+  console.log('isMouseDown: ' + isMouseDown);
+})
+
+
+//mouseenter -
+//mouseover -
+//mousedown - checks if user left clicks and holds 
+//mouseup - checks for when user releases that left click
+
+
 
 
 
